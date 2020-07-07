0844f01228b107bc9503350bdd137dc8f7638ead
Tim Davis <binary.god@gmail.com>
2020-07-07 16:06:50 -0400

    - Added new styles

--- Fuel_Calculator/PrUnTools_Fuel_Calculator.js
+++ Fuel_Calculator/PrUnTools_Fuel_Calculator.js
@@ -13,6 +13,14 @@
 (function() {
     'use strict';
 
+    let data;
+    let prices;
+    // REMOVE
+    // $.getJSON('market_data_multiple.json', (json) => {
+    //     data = json;
+    // });
+    // REMOVE
+
     // Load ApexUtils (Inserts Menu on APEX UI)
     apex.load();
 
@@ -28,16 +36,74 @@
         let stl = '';
         let ftl = '';
         let content = '';
+        let hasCurrency = false;
 
-        let prices = apex.state.comex.broker.brokers;
+        // Load prices from STATE
+        prices = apex.state.comex.broker.brokers;
 
+        // Make sure we have data
         if(Object.keys(prices).length > 0) {
 
+            // Get all entries with matching Ticker
             stl = Object.values(prices).filter(obj => { return obj.data.material.ticker === 'SF' });
-            ftl = Object.values(prices).filter(obj => { return obj.data.material.ticker === 'FF'});
+            ftl = Object.values(prices).filter(obj => { return obj.data.material.ticker === 'FF' });
 
+            // Ensure we have at least 1 of each Fuel type in the STATE data
             if(stl.length > 0 & ftl.length > 0) {
-                content = `
+
+                // Check if we have more than one entry per Fuel Type, if so then multiple currencies
+                // are involved.
+                if(stl.length > 1 | ftl.length > 1) {
+
+                    let currencies = {};
+                    // Add Name and Currency Code to Dictionary
+                    stl.forEach((item) => {
+                        currencies[item.data.currency.code] = item.data.currency.numericCode;
+                    });
+                    // Add Name and Currency Code to Dictionary
+                    ftl.forEach((item) => {
+                        currencies[item.data.currency.code] = item.data.currency.numericCode;
+                    });
+
+                    // We have multiple Currencies in STATE so we need to setup a way to pick the one the user
+                    // wants to use for calculations.
+                    content = `
+                        <table class='PrUnTools_Table'>
+                            <tbody>
+                                <tr>
+                                    <td class="PrUnTools_Label yellow" style="width:150px;">Currency</td>
+                                    <td class="PrUnTools_Label_Input yellow"><select class='PrUnTools_Control' id='currency-select'>
+                    `
+                    // Create our Options for Currencies that we have data for
+                    for(var key in currencies) {
+                        content += '<option value="' + currencies[key] + '">' + key + '</option>';
+                    };
+                    // Finish content
+                    content += `</select>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="top-border-cell" colspan="2">
+                                    <p>Multiple currencies found in STATE, select currency to use.</p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>`;
+
+                    // Default selected currency is 0, lets start with that one
+                    let selectedCode = currencies[Object.keys(currencies)[0]];
+                    stl = Object.values(prices).filter(obj => { return (obj.data.currency.numericCode === selectedCode & obj.data.material.ticker === 'SF') });
+                    ftl = Object.values(prices).filter(obj => { return (obj.data.currency.numericCode === selectedCode & obj.data.material.ticker === 'FF') });
+
+                    // Setup Currency Select Change event.
+                    $('BODY').on('change', '#currency-select', select_changed);
+
+                    // Mark that we have the currency stuff loaded (used to determine window size)
+                    hasCurrency = true;
+                }
+
+                // Add normal Calculator content
+                content += `
                 <table class="PrUnTools_Table">
                     <thead>
                         <tr>
@@ -68,33 +134,65 @@
                 </table>
             `;
 
-                $('body').on('keyup', '#sf-fuel-input', function(){
-                    $('#sf-fuel-total').html(($('#sf-fuel-input').val() * $('#sf-fuel-price').html()).toFixed(2));
-                    $('#fuel-total-cost').html((Number($('#sf-fuel-total').html()) + Number($('#ff-fuel-total').html())).toFixed(2));
-                });
-                $('body').on('keyup', '#ff-fuel-input', function(){
-                    $('#ff-fuel-total').html(($('#ff-fuel-input').val() * $('#ff-fuel-price').html()).toFixed(2));
-                    $('#fuel-total-cost').html((Number($('#sf-fuel-total').html()) + Number($('#ff-fuel-total').html())).toFixed(2));
-                });
+                // Setup events
+                $('body').on('keyup', '#sf-fuel-input', calculate);
+                $('body').on('keyup', '#ff-fuel-input', calculate);
 
+                // Configure initial price
                 if(stl[0].data.ask != null) {
                     content = content.replace('{{:stl_price}}', stl[0].data.ask.price.amount);
                 } else {
                     content = content.replace('{{:stl_price}}', 0);
                 }
-
+                // Configure initial price
                 if(ftl[0].data.ask != null) {
                     content = content.replace('{{:ftl_price}}', ftl[0].data.ask.price.amount);
                 } else {
                     content = content.replace('{{:ftl_price}}', 0);
                 }
 
-                apex.showBuffer('Fuel Calculator', 'PrUnTools', 300,118, content);
+                // Setup buffer default height
+                let bufferH = 118;
+                // If we are showing currency change it
+                if(hasCurrency) { bufferH = 180; }
+                // Finally show the calculator
+                apex.showBuffer('Fuel Calculator', 'PrUnTools', 300,300, content);
 
                 return null;
             }
         }
-
+        // Something is wrong!
         apex.showAlertBuffer('Fuel Calculator', 'PrUnTools', 'Price Data Missing', 'No Market data has been loaded to memory.  Navigate to area with market prices and use the MRKT utility.', 400,200, 'red');
+    }
+
+    function select_changed() {
+
+        // Get Selected Currency Code
+        let selectedCode = Number($('#currency-select option:selected').val());
+        // Get Fuel prices for that Code
+        let stl = Object.values(prices).filter(obj => { return (obj.data.currency.numericCode === selectedCode & obj.data.material.ticker === 'SF') });
+        let ftl = Object.values(prices).filter(obj => { return (obj.data.currency.numericCode === selectedCode & obj.data.material.ticker === 'FF') });
+        // Set HTML to new price
+        if(stl[0].data.ask != null) {
+            $('#sf-fuel-price').html(stl[0].data.ask.price.amount);
+        } else {
+            $('#sf-fuel-price').html(0);
+        }
+        // Set HTML to new price
+        if(ftl[0].data.ask != null)
+        {
+            $('#ff-fuel-price').html(ftl[0].data.ask.price.amount);
+        } else {
+            $('#ff-fuel-price').html(0);
+        }
+        // Calculate prices
+        calculate();
+    }
+
+    function calculate() {
+        // Calculate
+        $('#sf-fuel-total').html(($('#sf-fuel-input').val() * $('#sf-fuel-price').html()).toFixed(2));
+        $('#ff-fuel-total').html(($('#ff-fuel-input').val() * $('#ff-fuel-price').html()).toFixed(2));
+        $('#fuel-total-cost').html((Number($('#sf-fuel-total').html()) + Number($('#ff-fuel-total').html())).toFixed(2));
     }
 })();
